cf3eaadc89257b5c86e2c485877a9cba2fc5c3d5
schadix <schadix@gmx.net>
2011-06-26 09:26:41 -0400

    del biertoto

--- biertoto.sh
+++
@@ -1,1 +0,0 @@
-java -cp biertoto.jar:commons-cli-1.0.jar:jtidy.jar de.schadix.biertoto.dom.Test http://www.kicktipp.de/biertoto/tippuebersicht?kt_page=tippuebersicht\&sortBy=gesamtpunkte\&tippspieltagIndex=$1
--- doc/readme.txt
+++
@@ -1,6 +0,0 @@
-
-resources/kicktipp_xpath2009.properties: xpath values that point to the games or results
-
-Tipps:
-
-* To get the XPath values, try XPather Firefox plugin
